06d9202e8c2cc52bdf43a04bd5c9e4d95f6505f1
ADITI <aditi.raj.tech@gmail.com>
2026-06-07 20:57:17 +0530

    DAY7 : RECUSION REVISION

---
+++ DAY7/Power.java
@@ -0,0 +1,17 @@
+package DAY7;
+
+public class Power {
+    public static int power1(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+        return a * power1(a, b - 1);
+    }
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int result = power1(a, b);
+        System.out.println(a + " raised to the power of " + b + " is: " + result);
+    }
+}
